0b4e3130f19ce822e7984aae4de0c10e36e057da
vimalramk <vimalramk99@gmail.com>
2024-06-07 23:33:02 +0530

    feature UUID generator added

--- backend/src/routes/route.ts
+++ backend/src/routes/route.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response, Router } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const urlrouter: Router = express.Router();
+
+function generateUUID() {
+  const UUID = uuidv4().split("-")[0];
+  return UUID;
+}
 
 urlrouter.post("/v1/longurl/:url", (req: Request, res: Response) => {
   try {
     if (!req.params.url) {
       new Error("Please enter your url");
     }
+
     const longurl = req.params.url;
     console.log(longurl);
     res.send("Received");
