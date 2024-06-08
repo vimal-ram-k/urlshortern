c6935c90500c5b71768eed11001490ce04c36f4b
vimalramk <vimalramk99@gmail.com>
2024-06-08 07:43:43 +0530

    LongtoShort component tested.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { ChangeEvent, useState } from "react";
-import axios from "axios";
+import LongtoShort from "./component/LongtoShort";
 
 function App() {
-  const baseURL = "http://localhost:3000/v1/longurl";
-  const [longurl, setLongURL] = useState("");
-  const [shorturl, setShortURL] = useState("");
-
-  async function GenerateShortURL() {
-    const shortURL = await axios
-      .post(`${baseURL}/${longurl}`)
-      .then((response) => {
-        return response.data.shorturl;
-      });
-    setShortURL(shortURL);
-  }
-
   return (
     <>
-      <div>
-        <label htmlFor="longurl">Enter your Orginal URL</label>
-        <input
-          type="text"
-          name="longurl"
-          id="longurl"
-          value={longurl}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setLongURL(e.target.value)
-          }
-        />
-        <button onClick={GenerateShortURL}>Generate</button>
-
-        <input type="text" value={shorturl} contentEditable={false} />
-      </div>
+      <LongtoShort />
     </>
   );
 }
